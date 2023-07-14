d2d956d098585344db3eacab8cbf3945ab044847
MinsuJeon <chso294@naver.com>
2023-07-14 18:47:43 +0900

    feat: ReuseIdentifiable 프로토콜 생성 및 적용
    - CollectionView Cell 등록시 ID 관련 boilerplate 제거를 위한 protocol ReuseIdentifiable 생성 및 적용

---
+++ FoodRecommendation/FoodRecommendation/Extension/UICollectionView+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UICollectionView+Extension.swift
+//  FoodRecommendation
+//
+//  Created by 전민수 on 2023/07/13.
+//
+
+import UIKit
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(cellType: T.Type) {
+        register(cellType.self, forCellWithReuseIdentifier: T.identifier)
+    }
+
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(
+            withReuseIdentifier: T.identifier,
+            for: indexPath
+        ) as? T else {
+            fatalError("Failed to dequeue a cell with identifier \(T.identifier) matching Type \(T.self)")
+        }
+
+        return cell
+    }
---
+++ FoodRecommendation/FoodRecommendation/Protocol/ReuseIdentifiable.swift
@@ -0,0 +1,20 @@
+//
+//  ReuseIdentifiable.swift
+//  FoodRecommendation
+//
+//  Created by 전민수 on 2023/07/13.
+//
+
+import UIKit
+
+protocol ReuseIdentifiable {
+    static var identifier: String { get }
+}
+
+extension ReuseIdentifiable {
+    static var identifier: String {
+        return String(describing: Self.self)
+    }
+}
+
+extension UICollectionViewCell: ReuseIdentifiable { }

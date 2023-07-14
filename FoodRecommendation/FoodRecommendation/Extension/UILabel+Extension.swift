5b419caa20b045b2b2bdd512f9ec2f4c787e38f2
MinsuJeon <chso294@naver.com>
2023-07-14 18:45:23 +0900

    feat: SettingsCell 및 SettingsCellButton를 위한 extension 추가
    - 원활한 SettingsCellButton UI 설정을 위한 extension UILabel 추가
    - 원활한 SettingsCell UI 설정을 위한 extension UIView 추가

---
+++ FoodRecommendation/FoodRecommendation/Extension/UILabel+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UILabel+Extension.swift
+//  FoodRecommendation
+//
+//  Created by 전민수 on 2023/07/14.
+//
+
+import UIKit
+
+extension UILabel {
+    func configureUI(text: String = "", textColor: UIColor = .black, textAlignment: NSTextAlignment = .left, font: UIFont = .preferredFont(forTextStyle: .body)) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.text = text
+        self.textColor = textColor
+        self.textAlignment = textAlignment
+        self.font = font
+    }
+}

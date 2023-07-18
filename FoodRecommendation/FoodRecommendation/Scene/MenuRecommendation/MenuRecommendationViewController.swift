931c035fc728114dff2b152e4b4ce4b4396ea157
MinsuJeon <chso294@naver.com>
2023-07-18 15:49:38 +0900

    feat: MenuRecommendationViewController 생성
    - 메뉴 추천을 위한 ViewController 생성

---
+++ FoodRecommendation/FoodRecommendation/Scene/MenuRecommendation/MenuRecommendationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MenuRecommendationViewController.swift
+//  FoodRecommendation
+//
+//  Created by 전민수 on 2023/07/18.
+//
+
+import UIKit
+
+final class MenuRecommendationViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

a7755a28d48af595685385d8dbf584ce725e5edf
MinsuJeon <chso294@naver.com>
2023-07-19 22:08:25 +0900

    feat: SelectedMenuViewController 생성
    - 사용자가 최종 선택한 메뉴를 표시하는 ViewController 생성

---
+++ FoodRecommendation/FoodRecommendation/Scene/SelectedMenu/SelectedMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectedMenuViewController.swift
+//  FoodRecommendation
+//
+//  Created by 전민수 on 2023/07/19.
+//
+
+import UIKit
+
+final class SelectedMenuViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

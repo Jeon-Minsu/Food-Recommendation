3a2f498959426b552230ae900939fb15e74187ab
MinsuJeon <chso294@naver.com>
2023-07-19 20:05:00 +0900

    refactor: UIImageView extension 수정
    - 함수의 파라미터로 alpha 인자 추가

--- FoodRecommendation/FoodRecommendation/Extension/UIImageView+Extension.swift
+++ FoodRecommendation/FoodRecommendation/Extension/UIImageView+Extension.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 extension UIImageView {
-    func configureUI(image: UIImage? = nil, isSizeNeedToFit: Bool = false) {
+    func configureUI(image: UIImage? = nil, alpha: CGFloat = 1, isSizeNeedToFit: Bool = false) {
         self.translatesAutoresizingMaskIntoConstraints = false
+        self.alpha = alpha
         self.image = image
 
         if isSizeNeedToFit {

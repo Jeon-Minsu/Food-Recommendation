7ec9c208b5564bd222f7f74a30b601d9926d5a48
MinsuJeon <chso294@naver.com>
2023-07-19 23:15:56 +0900

    feat: 사용자가 선택한 메뉴를 제시하는 UILabel 생성
    - SelectedMenuViewController 내 firstDescriptionLabel,  menuLabel, secondDescriptionLabel 추가
    - 각각의 UILable UI 설정
    - 시간에 따른 끼니 문자열을 반환하는 getEatingTimePhrase() 함수 생성 및 적용

--- FoodRecommendation/FoodRecommendation/Scene/SelectedMenu/SelectedMenuViewController.swift
+++ FoodRecommendation/FoodRecommendation/Scene/SelectedMenu/SelectedMenuViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 final class SelectedMenuViewController: UIViewController {
 
+    // MARK: Properties
+    private let firstDescriptionLabel = UILabel()
+    private let menuLabel = UILabel()
+    private let secondDescriptionLabel = UILabel()
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureHierarchy()
+    private func configureHierarchy() {
+        configureUI()
+    }
+    private func configureUI() {
+        firstDescriptionLabel.configureUI(text: "오늘 \(getEatingTimePhrase()) 메뉴는", textAlignment: .left)
+        menuLabel.configureUI(text: "새우 초밥", textAlignment: .center, font: .preferredFont(forTextStyle: .largeTitle))
+        secondDescriptionLabel.configureUI(text: "이 좋겠군...", textAlignment: .right)
+    private func getEatingTimePhrase() -> String {
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+
+        switch hour {
+        case 6..<10:
+            return "아침"
+        case 12..<15:
+            return "점심"
+        case 17..<21:
+            return "저녁"
+        case 21..<24, 0..<3:
+            return "야식"
+        default:
+            return "간식"
+        }
+    }
     }
 }

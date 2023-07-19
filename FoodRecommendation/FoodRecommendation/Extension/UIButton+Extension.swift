89c8f029e140f12302fbfd34d88b73afdbfc16e7
MinsuJeon <chso294@naver.com>
2023-07-19 20:05:31 +0900

    refactor: UIButton extension 수정
    - 함수의 파라미터로 titleColor, tintColor 인자 추가

--- FoodRecommendation/FoodRecommendation/Extension/UIButton+Extension.swift
+++ FoodRecommendation/FoodRecommendation/Extension/UIButton+Extension.swift
@@ -8,9 +8,11 @@
 import UIKit
 
 extension UIButton {
-    func configureUI(title: String, state: UIControl.State = .normal, backgroundColor: UIColor = .systemBackground, cornerRadius: CGFloat = 0) {
+    func configureUI(title: String, state: UIControl.State = .normal, titleColor: UIColor = .black, tintColor: UIColor = .black, backgroundColor: UIColor = .systemBackground, cornerRadius: CGFloat = 0) {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.setTitle(title, for: state)
+        self.setTitleColor(titleColor, for: .normal)
+        self.tintColor = tintColor
         self.backgroundColor = backgroundColor
         self.layer.cornerRadius = cornerRadius
     }

6a23329cc8f320af06e421689d1ecd012fdb8f3a
MinsuJeon <chso294@naver.com>
2023-07-19 19:50:41 +0900

    feat: LogoImageView UI 구현
    - MenuRecommendationViewController 내 logoImageView 추가
    - logoImageView UI 설정

--- FoodRecommendation/FoodRecommendation/Scene/MenuRecommendation/MenuRecommendationViewController.swift
+++ FoodRecommendation/FoodRecommendation/Scene/MenuRecommendation/MenuRecommendationViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 final class MenuRecommendationViewController: UIViewController {
 
+    // MARK: Properties
+
+    private let logoImageView = UIImageView()
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureHierarchy()
+
+    // MARK: - Methods
+
+    private func configureHierarchy() {
+        view.backgroundColor = .systemBackground
+        navigationItem.hidesBackButton = true
+        configureUI()
+    }
+    private func configureUI() {
+        logoImageView.configureUI(image: UIImage(systemName: "star.fill"))
+        setupLogoImageViewUI()
+    private func setupLogoImageViewUI() {
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 40),
+            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            logoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    }
     }
 }

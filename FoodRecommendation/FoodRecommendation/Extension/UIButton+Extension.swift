71b55675e52e491e4cbe9eb21aa42b1349ac0500
MinsuJeon <chso294@naver.com>
2023-07-24 22:37:12 +0900

    refactor: UIButton extension 수정
    - font 및 image 설정 기능 추가
    - backgroundColor 및 tintColor에 대한 조건문 추가

--- FoodRecommendation/FoodRecommendation/Extension/UIButton+Extension.swift
+++ FoodRecommendation/FoodRecommendation/Extension/UIButton+Extension.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 extension UIButton {
-    func configureUI(title: String, state: UIControl.State = .normal, titleColor: UIColor = .black, tintColor: UIColor = .black, backgroundColor: UIColor = .clear, cornerRadius: CGFloat = 0) {
+    func configureUI(title: String = "", state: UIControl.State = .normal, font: UIFont? = .preferredFont(forTextStyle: .body), titleColor: UIColor = .black, tintColor: UIColor? = .black, backgroundColor: UIColor? = .clear, cornerRadius: CGFloat = 0, image: UIImage? = nil) {
+        guard let backgroundColor = backgroundColor, let tintColor = tintColor else {
+            return
+        }
+
         self.translatesAutoresizingMaskIntoConstraints = false
         self.setTitle(title, for: state)
+        self.titleLabel?.font = font
         self.setTitleColor(titleColor, for: .normal)
         self.tintColor = tintColor
         self.backgroundColor = backgroundColor
         self.layer.cornerRadius = cornerRadius
+        self.setImage(image, for: .normal)
     }
 }

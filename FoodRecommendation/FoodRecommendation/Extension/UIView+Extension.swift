64479362bcd9aeeac5651cacc4c4fc476907082e
MinsuJeon <chso294@naver.com>
2023-07-24 22:57:32 +0900

    feat: UIView extension 기능 추가
    - 그라데이션 배경색 및 백그라운드 이미지를 등록하는 setGradientBackground 함수 구현

--- FoodRecommendation/FoodRecommendation/Extension/UIView+Extension.swift
+++ FoodRecommendation/FoodRecommendation/Extension/UIView+Extension.swift
@@ -16,4 +16,30 @@ extension UIView {
         self.layer.borderColor = borderColor
         self.backgroundColor = backgroundColor
     }
+
+    func setGradientBackground(startColor: UIColor?, endColor: UIColor?, patternImage: UIImage?) {
+        guard let startColor = startColor, let endColor = endColor else {
+            backgroundColor = .systemBackground
+            return
+        }
+
+        let gradientColors = [startColor.cgColor, endColor.cgColor]
+        let gradientLocations: [NSNumber] = [0.0, 1.0]
+        let startPoint = CGPoint(x: 0.5, y: 0.0)
+        let endPoint = CGPoint(x: 0.5, y: 1.0)
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
+        gradientLayer.frame = bounds
+
+        layer.addSublayer(gradientLayer)
+
+        let patternImageView = UIImageView(image: patternImage)
+        patternImageView.contentMode = .scaleAspectFill
+        patternImageView.frame = bounds
+
+        addSubview(patternImageView)
+    }
 }

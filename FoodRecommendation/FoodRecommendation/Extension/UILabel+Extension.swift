2a8cecfb99a2ec5b87f91f281242370b96e2e8af
MinsuJeon <chso294@naver.com>
2023-07-27 02:09:24 +0900

    refactor: UILabel extension 수정
    - numberOfLines 인자 추가

--- FoodRecommendation/FoodRecommendation/Extension/UILabel+Extension.swift
+++ FoodRecommendation/FoodRecommendation/Extension/UILabel+Extension.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 extension UILabel {
-    func configureUI(text: String = "", textColor: UIColor? = .black, textAlignment: NSTextAlignment = .left, font: UIFont? = .preferredFont(forTextStyle: .body), alpha: CGFloat = 1) {
+    func configureUI(text: String = "", textColor: UIColor? = .black, textAlignment: NSTextAlignment = .left, font: UIFont? = .preferredFont(forTextStyle: .body), numberOfLines: Int = 1, alpha: CGFloat = 1) {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.text = text
         self.textColor = textColor
         self.textAlignment = textAlignment
         self.font = font
+        self.numberOfLines = numberOfLines
         self.alpha = alpha
     }
 }
